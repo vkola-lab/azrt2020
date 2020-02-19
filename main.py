4779e5a1379d9ae36d28addfef7fd3321a07ada9
shangranq <shangran@bu.edu>
2020-02-19 13:09:51 -0500

    build whole pipeline

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def gan_main():
+    # gan training, record niqe, image, SSIM, brisque, ... over time on validation set
+    # after training, generate 1.5T* for CNN /data/datasets/ADNIP_NoBack/
+    # ...
+
+
+def cnn_main():
+    # 5 random splits on 1.5T and 1.5T*
+    # generate SS (ROC) and PR curves
+    # print CNN performance table, accu, sens, spec, f1, ... 
+
+
